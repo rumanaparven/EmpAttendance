21e7bf45c60a4695bd257728e84bb6a36022a041
Rumana Parven <rumanaparven16@gmail.com>
2020-09-27 06:53:31 -0700

    [Rumana] Add. Employee wages using switch case

--- EmpWages/Program.cs
+++ EmpWages/Program.cs
@@ -4,26 +4,26 @@ namespace EmpWages
 {
     class Program
     {
+        public const int is_part_time = 1;
+        public const int is_full_time = 2;
+        public const int rate_per_hour = 20;
         static void Main(string[] args)
         {
-            int is_part_time = 1;
-            int is_full_time = 2;
-            int rate_per_hour = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             int check = random.Next(0, 3);
-            if (check == is_part_time)
+            switch (check)
             {
-                empHrs = 4;
-            }
-            else if (check == is_full_time)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0;
+                case is_part_time:
+                    empHrs = 4;
+                    break;
+                case is_full_time:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * rate_per_hour;
             Console.WriteLine("Emp Wage : " + empWage);
