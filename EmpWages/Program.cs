8a4225cf163d6fdda4b837fbe59cbbc532c3e32f
Rumana Parven <rumanaparven16@gmail.com>
2020-09-27 06:45:57 -0700

    [Rumana] Add. Part time employee wage

--- EmpWages/Program.cs
+++ EmpWages/Program.cs
@@ -6,14 +6,18 @@ namespace EmpWages
     {
         static void Main(string[] args)
         {
-
-            int is_full_time = 1;
+            int is_part_time = 1;
+            int is_full_time = 2;
             int rate_per_hour = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int check = random.Next(0, 2);
-            if (check == is_full_time)
+            int check = random.Next(0, 3);
+            if (check == is_part_time)
+            {
+                empHrs = 4;
+            }
+            else if (check == is_full_time)
             {
                 empHrs = 8;
             }
