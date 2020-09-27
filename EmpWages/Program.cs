ae78862e65400a032a35e9fc2357dc41e9a651b0
Rumana Parven <rumanaparven16@gmail.com>
2020-09-27 07:07:34 -0700

    [Rumana] Add. Calculation of wages for a month.

--- EmpWages/Program.cs
+++ EmpWages/Program.cs
@@ -7,26 +7,34 @@ namespace EmpWages
         public const int is_part_time = 1;
         public const int is_full_time = 2;
         public const int rate_per_hour = 20;
+        public const int num_of_working_days = 2;
         static void Main(string[] args)
         {
+
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
-            int check = random.Next(0, 3);
-            switch (check)
-            {
-                case is_part_time:
-                    empHrs = 4;
-                    break;
-                case is_full_time:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+            int totalEmpWage = 0;
+            for (int day = 0; day < num_of_working_days; day++)
+            { 
+                Random random = new Random();
+                int check = random.Next(0, 3);
+                switch (check)
+                {
+                    case is_part_time:
+                        empHrs = 4;
+                        break;
+                    case is_full_time:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * rate_per_hour;
+                totalEmpWage = totalEmpWage + empWage;
+                Console.WriteLine("Emp Wage : " + empWage);
             }
-            empWage = empHrs * rate_per_hour;
-            Console.WriteLine("Emp Wage : " + empWage);
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
         }
     }
 }
