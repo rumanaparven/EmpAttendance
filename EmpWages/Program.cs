7d5f53e113f78c61b8959b6cd0cbb0219a2fe7d3
Rumana Parven <rumanaparven16@gmail.com>
2020-09-27 06:27:39 -0700

    [Rumana] Add. Welcome message.

---
+++ EmpWages/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmpWages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Welcome to attendance checker");
+        }
+    }
+}
